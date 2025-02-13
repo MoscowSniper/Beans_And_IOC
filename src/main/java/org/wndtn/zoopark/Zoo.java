b8b3860509187301653d4996ade0624455e3e6bc
Богдан <boganan33@gmail.com>
2025-02-13 17:38:27 +0300

    Zoo class updated

--- src/main/java/org/wndtn/zoopark/Zoo.java
+++ src/main/java/org/wndtn/zoopark/Zoo.java
@@ -1,7 +1,6 @@
 package org.wndtn.zoopark;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -11,15 +10,8 @@ public class Zoo {
     private final List<Animal> animals;
 
     @Autowired
-    public Zoo(@Qualifier("lion") Animal lion,
-               @Qualifier("parrot") Animal parrot,
-               @Qualifier("elephant") Animal elephant,
-               @Qualifier("monkey") Animal monkey,
-               @Qualifier("cat") Animal cat,
-               @Qualifier("dog") Animal dog,
-               @Qualifier("cow") Animal cow) {
-        this.animals = List.of(lion, parrot, elephant, monkey, cat, dog, cow);
-
+    public Zoo(List<Animal> animals) {
+        this.animals = animals;
     }
 
     public void makeSounds() {
